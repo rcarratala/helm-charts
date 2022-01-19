5d99f9e022b3f3ce5d72d7c9ecb251fcb71cee33
rcarrata <rcarratalasanchez@gmail.com>
2022-01-19 16:14:49 +0100

    added more stuff

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-helm upgrade --install bootstrap-projects bootstrap-projects
-
-helm upgrade --install argocd-operator argocd-operator --set namespace=argocd
-
-oc apply -n argocd -f argocd-install
-
-helm upgrade --install parks-app parks-app
